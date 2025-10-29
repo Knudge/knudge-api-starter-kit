8eb58b013ba7e3fbaebca0d83eb5e642b6a481fc
Logan Moore <logan@knudge.com>
2025-10-29 18:02:01 -0500

    Add refresh example

--- api/routes/oauth.mjs
+++ api/routes/oauth.mjs
@@ -16,6 +16,12 @@ export default {
     'DELETE': {
       handle: handleUnlink
     },
+    'PATCH': {
+      handle: handleRefresh,
+      searchParamsSchema: joi.object({
+        client_id: joi.string()
+      }),
+    },
     'POST': {
       handle: handleLink,
       bodySchema: joi.object({
@@ -46,15 +52,15 @@ async function handleLink(ctx) {
     let { authorizationBase64 } = await getClient(ctx);
 
     tokenResult = await fetch(`${ KNUDGE_ORIGIN_API }/v1/oauth/token`, {
+      body: new URLSearchParams({
+        code: body.code,
+        grant_type: 'authorization_code'
+      }).toString(),
       headers: {
         'authorization': `basic ${ authorizationBase64 }`,
         'accept': 'application/json',
         'content-type': 'application/x-www-form-urlencoded'
       },
-      body: new URLSearchParams({
-        code: body.code,
-        grant_type: 'authorization_code'
-      }).toString(),
       method: 'POST'
     });
   } catch (err) {
@@ -98,3 +104,61 @@ async function handleUnlink(ctx) {
   ctx.cookies.set('sesh', null);
   await kvStore.remove(`session-${ cookie }`);
 }
+
+/**
+ * Example of how to refresh an API token. This would normally be done by some
+ * 2-week cron job, or if the current access token is determined to be expired
+ * when making an API request to Knudge, but we wrap it up in an endpoint to
+ * make it easier to play around with.
+ *
+ * @param {import('koa').Context} ctx
+ */
+async function handleRefresh(ctx) {
+  let tokenResult;
+
+  try {
+    let { authorizationBase64 } = await getClient(ctx);
+    let { refresh_token } = JSON.parse(await kvStore.read(`sesh-${ cookie }`));
+
+    tokenResult = await fetch(`${ KNUDGE_ORIGIN_API }/v1/oauth/token`, {
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token
+      }).toString(),
+      headers: {
+        'authorization': `basic ${ authorizationBase64 }`,
+        'accept': 'application/json',
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST'
+    });
+  } catch (err) {
+    console.error(util.inspect(err, { depth: Infinity, colors: true }))
+  }
+
+  if (!tokenResult?.ok) {
+    console.error(util.inspect(tokenResult, { depth: Infinity, colors: true }))
+
+    if (tokenResult?.body) {
+      console.error(await tokenResult.text());
+    }
+
+    return ctx.throw(
+      tokenResult?.status ?? 500,
+      `Knudge response: ${ tokenResult?.statusText ?? 'Internal Server Error' }`
+    );
+  }
+
+  if (tokenResult.bodyUsed)
+    throw new Error('Body used');
+
+  const tokenJSON = await tokenResult.json();
+  const cookie = generateCookie();
+
+  // Update our copy of the token with the latest data
+  await kvStore.write(`sesh-${ cookie }`, JSON.stringify(tokenJSON, null, 2));
+
+  ctx.status = 200;
+  ctx.body = {};
+}
+
